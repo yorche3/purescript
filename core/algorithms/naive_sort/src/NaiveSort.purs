6c060765d5059e475674e54f224527c9051ef5e0
yorche3 <hyaoki123@gmail.com>
2026-09-18 19:20:57 -0600

    feat(algorithms): add naive sort implementation no idiomatic

--- core/algorithms/naive_sort/src/NaiveSort.purs
+++ core/algorithms/naive_sort/src/NaiveSort.purs
@@ -10,3 +10,47 @@
 --   insertionSort(arr)
 
 module NaiveSort where
+
+selectionSort :: Array Int -> Array Int
+selectionSort [] = []
+selectionSort [x] = [x]
+selectionSort (x:xs) =
+    let
+      pickMin x (y:ys) =
+        case (y:ys) of
+          [] -> (x, [])
+          (y:ys') ->
+            if x <= y then (x, y:ys')
+            else
+              let (minVal, rest) = pickMin x ys'
+              in (minVal, y:rest)
+    (minVal, rest) = pickMin x xs
+  in minVal : selectionSort rest
+
+bubbleSort :: Array Int -> Array Int
+bubbleSort [] = []
+bubbleSort [x] = [x]
+bubbleSort (x:xs) =
+  let
+    bubble [] = ([], False)
+    bubble [y] = ([y], False)
+    bubble (y:z:ys) =
+      if y > z then
+        let (rest, swapped) = bubble (y:ys)
+        in (z:rest, True)
+      else
+        let (rest, swapped) = bubble (z:ys)
+        in (y:rest, swapped)
+    (bubbled, swapped) = bubble (x:xs)
+  in if swapped then bubbleSort bubbled else bubbled
+
+insertionSort :: Array Int -> Array Int
+insertionSort [] = []
+insertionSort [x] = [x]
+insertionSort (x:xs) =
+  let
+    insert y [] = [y]
+    insert y (z:zs) =
+      if y <= z then y : z : zs
+      else z : insert y zs
+  in insert x (insertionSort xs)
